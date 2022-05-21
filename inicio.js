e36f33612a2c4504d4a4f713e8557267fab70073
jigalindo <josegalindo98@unitec.edu>
2022-05-21 15:42:33 -0600

    first commiit

--- inicio.js
+++ inicio.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const rutas=  require('./canciones');
+const rutas=  require('./videos');
 var app = express();
 const bodyParser = require("body-parser");
 var path = require('path');
@@ -13,7 +13,6 @@ app.use('/api',rutas);
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname,'pagina.html'));
-  //res.status(200).send('Bienvenido a Nuestra Tienda de Musica!');
 });
 
 
